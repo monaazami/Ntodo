928fc905ee44c9b46667281d99b4843eec91af1e
monaazami <32145698+monaazami@users.noreply.github.com>
2018-10-21 00:32:49 +0100

    add delete functionality

--- todo.js
+++ todo.js
@@ -4,12 +4,21 @@ var input = prompt('What would you like to do?');
 
 while (input !== 'quit') {
 	if (input === 'list') {
-		console.log(todos);
+        todos.forEach(function(todo,index){
+            console.log('**********')
+            console.log(index +':' +todo)
+            console.log('**********')
+        })
+		// console.log(todos);
 	} else if (input === 'new') {
 		var newTodo = prompt('Enter new todo');
 		todos.push(newTodo);
+    }else if(input === 'delete'){
+        var deleted= prompt('Which index do you want to be deleted?')
+            todos.splice(deleted,1)
+        
+
     }
     input = prompt('What would you like to do?');
 }
 console.log('Cool, you quit the app!');
-x
