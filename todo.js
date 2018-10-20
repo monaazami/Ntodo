34b43c00a3da142202ed90bc44e95d41d9a9b03a
monaazami <32145698+monaazami@users.noreply.github.com>
2018-10-21 00:49:59 +0100

    make codes neat and tidy

--- todo.js
+++ todo.js
@@ -4,21 +4,32 @@ var input = prompt('What would you like to do?');
 
 while (input !== 'quit') {
 	if (input === 'list') {
-        todos.forEach(function(todo,index){
-            console.log('**********')
-            console.log(index +':' +todo)
-            console.log('**********')
-        })
-		// console.log(todos);
+        listTodo()
 	} else if (input === 'new') {
-		var newTodo = prompt('Enter new todo');
-		todos.push(newTodo);
+		newTodo()
     }else if(input === 'delete'){
-        var deleted= prompt('Which index do you want to be deleted?')
-            todos.splice(deleted,1)
-        
-
+        deleteTodo()
     }
     input = prompt('What would you like to do?');
 }
 console.log('Cool, you quit the app!');
+
+
+
+function listTodo(){
+    todos.forEach(function(todo,index){
+        console.log('**********')
+        console.log(index +':' +todo)
+        console.log('**********')
+    })
+}
+
+function newTodo(){
+    var newT = prompt('Enter new todo');
+    todos.push(newT);
+}
+
+function deleteTodo(){
+    var deleted= prompt('Which index do you want to be deleted?')
+    todos.splice(deleted,1)
+}
